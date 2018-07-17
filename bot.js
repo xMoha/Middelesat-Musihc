227309c7ddb43222d23d67ed39f593872c38851a
xMoha <40671043+xMoha@users.noreply.github.com>
2018-07-17 17:47:42 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -10,7 +10,7 @@ const fetchVideoInfo = require('youtube-info');
 const YouTube = require('simple-youtube-api');
 const youtube = new YouTube("AIzaSyAdORXg7UZUo7sePv97JyoDqtQVi3Ll0b8");
 const queue = new Map();
-const prefix = '-'
+const prefix = '='
 const client = new Discord.Client();
 
 client.on('ready', () => {
